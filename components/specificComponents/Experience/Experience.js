fd9dc98cab9474f64b7b0b5e421ff73b1f1723f3
creatingvaluewithwebsites-ESP <creatingvaluewithwebsites@gmail.com>
2026-01-15 11:31:27 +0100

    Change of Experience.js and de .scss

--- components/specificComponents/Experience/Experience.js
+++ components/specificComponents/Experience/Experience.js
@@ -1,27 +1,18 @@
 ﻿import React, { Component } from "react";
 import css from "./Experience.module.scss";
 import { storyblokEditable } from "@storyblok/react";
-import {RichTextToHTML} from "../../../functions/storyBlokRichTextRenderer";
 
 export default class Experience extends Component {
-
-	constructor(props) {
-		super(props);
-	}
-
-	render() {
-		return (
-			<>
-				<div {...storyblokEditable(this.props.blok)} className={css["experienceitem"]}>
-					<div className={css["experienceheader"]}>
-						<span className={css["experiencedate"]}>{this.props.blok.startdate} - {this.props.blok.enddate}</span>
-						<span className={css["experiencetitle"]}>{this.props.blok.title}</span>
-					</div>
-					<div className={css["experienceitemcontent"]}>
-						{RichTextToHTML({ document: this.props.blok.description })}
-					</div>
-				</div>
-			</>
-		);
-	}
+    render() {
+        return (
+            <div {...storyblokEditable(this.props.blok)} className={css["experience-wrapper"]}>
+                <div className={css["experience-card"]}>
+                    {/* El contenido del texto */}
+                    <div className={css["experience-content"]}>
+                        {this.props.blok.experience || "No description provided."}
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
